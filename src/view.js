7eb9d38de81add154da7c27edb340946f03c22d2
Evy-Oliveira <evelyn.o-s@hotmail.com>
2024-02-02 20:05:11 -0300

    adequação do view na linha 9, para poder estilizar cada classificação

--- src/view.js
+++ src/view.js
@@ -6,23 +6,23 @@ export const renderItems = (data) => {
     cartoes.innerHTML += `
     <li class="cartao" itemscope itemtype="${item.id}">
       <section class="info-externa">
-        <p><span itemprop="classification">${item.extraInfo.classification}</span></p>
-        <figure> <img  itemprop="imageUrl" src="${item.imageUrl}"  title="${item.name}"></figure>
-        <h5><span itemprop="name">${item.name}</span></h5>
-        <p><span itemprop="assessment">${parseFloat(item.extraInfo.assessment)}</span></p>
-        <p><span itemprop="streaming">${item.extraInfo.streaming}</span></p>
-        <p>Temporadas <span itemprop="seasons">${item.extraInfo.seasons}</span></p>
-        <button onclick="openModal('${item.id}')">Curiosidades</button>
+        <p class="classificacao cl-${item.extraInfo.classification}"><span itemprop="classification">${item.extraInfo.classification}</span></p>
+        <figure> <img class"imgAnime" itemprop="imageUrl" src="${item.imageUrl}"  title="${item.name}"></figure>
+        <h5 class="nomeAnime"><span itemprop="name">${item.name}</span></h5>
+        <p class="avaliacao"><span itemprop="assessment">${parseFloat(item.extraInfo.assessment)}</span></p>
+        <p class="plataforma"><span itemprop="streaming">${item.extraInfo.streaming}</span></p>
+        <p class="temporadas">Temporadas <span itemprop="seasons">${item.extraInfo.seasons}</span></p>
+        <button class="botaoModal" onclick="openModal('${item.id}')">Curiosidades</button>
       </section>
       <section class="curiosidades" id="modal-${item.id}">
         <span class="close" onclick="closeModal('${item.id}')">&times;</span>
-        <h6><span itemprop="shortDescription">${item.shortDescription}</span ></h6 >
-        <p><span itemprop="description">${item.description}</span>.</p>
-        <p>Episódios: <span itemprop="episodes">${item.extraInfo.episodes}</span></p>
-        <p>Canção: <span itemprop="song">${item.extraInfo.song.join(", ")}</span></p>
-        <p>Criador: <span itemprop="creator">${item.facts.creator}</span></p>
-        <p>Produtor: <span itemprop="producer">${item.facts.producer}</span></p>
-        <p>Studio: <span itemprop="studio">${item.facts.studio}</span></p>
+        <h6 class="genero"><span itemprop="shortDescription">${item.shortDescription}</span ></h6 >
+        <p class="descricao"><span itemprop="description">${item.description}</span>.</p>
+        <p class="episodios">Episódios: <span itemprop="episodes">${item.extraInfo.episodes}</span></p>
+        <p class="musica">Canção: <span itemprop="song">${item.extraInfo.song.join(", ")}</span></p>
+        <p class="criador">Criador: <span itemprop="creator">${item.facts.creator}</span></p>
+        <p class="produtor">Produtor: <span itemprop="producer">${item.facts.producer}</span></p>
+        <p class="studio">Studio: <span itemprop="studio">${item.facts.studio}</span></p>
       </section >
     </li >
   `;
