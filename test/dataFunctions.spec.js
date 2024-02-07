25484cebd8bac5c1959bf9d48b7c2c662d7b6686
flaviane-lima <rflaviane41@gmail.com>
2024-02-06 22:04:48 -0300

    foi escrito  o teste da função filtrar, ordenar e estatistica

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,20 +1,62 @@
-import { example, anotherExample } from '../src/dataFunctions.js';
-import { data as fakeData } from './data.js';
+import { filterBy, sortBy, computeStats} from '../src/dataFunctions.js';
+import { data as fakeData, mockOriginal, mockCrescente, mockDecrescente} from './data.js';
 
 // console.log(fakeData);
 
-describe('example', () => {
-  fakeData
 
-  it('returns `example`', () => {
+
+describe('filterby', () => {
+  // fakeData
+
+  it('filtra dados por plataforma de streaming', () => {
     fakeData.length;
-    expect(example()).toBe('example');
+    // let comparacaoStreaming = netflix
+    const filteredData = filterBy(fakeData, 'streaming', 'netflix');
+    expect(filteredData.every(item => item.extraInfo.streaming === 'netflix')).toBe(true);
   });
+  // it('lida com a propriedade extraInfo ou filterBy ausente', () => {
+  //   const filteredData = filterBy([{ name: 'Cardcaptor Sakura' }], 'streaming', 'netflix');
+
+  //   // Verifica se a função lida corretamente com dados que não possuem a estrutura esperada
+  //   expect(filteredData).toEqual([]);
+  // });
+  
+});
+describe('computeStats', () => {
+  it('Calculando as estatisticas de acordo com a classificação', () => {
+    const resultado = computeStats(fakeData);
+    const resultadoEsperado = {
+      L: 20.0,
+      // A10: 4.17,
+      // A12: 33.33,
+      A12: 20.0,
+      A14: 20.0,
+      A16: 40.0,
+      // A18: 8.33,
+    }
+    expect(resultado).toEqual(resultadoEsperado);
+  });
+  it('A soma das porcentagens deve ser 100', () => {
+    const resultado = computeStats(fakeData);
+    let somaPorcentagens = 0;
+    for (const classificacao in resultado) {
+      somaPorcentagens += resultado[classificacao];
+    }
+    expect(somaPorcentagens).toBe(100);
+  })
 });
 
-describe('anotherExample', () => {
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+
+describe('sortBy', () => {
+
+  it('ordenação de dados pelos os anime de A-Z', () => {
+    const ordenarData = sortBy(fakeData, 'name', 'asc')
+    // console.log(ordenarData);
+    expect(ordenarData).toStrictEqual(mockCrescente);
   });
+  it('ordenação de dados pelos os animes de Z-A', () => {
+    const ordenarData = sortBy(fakeData, 'name', 'desc')
+    expect(ordenarData).toStrictEqual(mockDecrescente);
+  })
 });
